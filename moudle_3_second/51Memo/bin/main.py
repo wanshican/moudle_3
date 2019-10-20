2f436db9d3867a131843e545fcae652dfcb09adf
wanshican <wanshican@163.com>
2019-10-20 12:48:12 +0800

    所有功能可以通过命令行参数形式调用

--- moudle_3_second/51Memo/bin/main.py
+++ moudle_3_second/51Memo/bin/main.py
@@ -7,7 +7,29 @@ from core import memo, log_function
 
 
 def main():
+    try:
+        ma = memo.MemoAdmin()
+        ma.login()
+        if sys.argv[1] in {'-a', '--add'}:
+            ma.add()
+        elif sys.argv[1] in {'-d', '--delete'}:
+            ma.dele()
+        elif sys.argv[1] in {'-m', '--modify'}:
+            ma.modify()
+        elif sys.argv[1] in {'-q', '--query'}:
+            ma.query()
+        elif sys.argv[1] in {'-s', '--save'}:
+            ma.save()
+        elif sys.argv[1] in {'-e', '--export'}:
+            ma.export_pdf()
+        elif sys.argv[1] in {'-qm', '--query_memo'}:
+            ma.query_memo()
+        elif sys.argv[1] in {'-se', '--send_email'}:
+            ma.send_email()
+    except Exception as e:
+        print(e)
     memo.main()
+    
 
 if __name__ == "__main__":
     main()
